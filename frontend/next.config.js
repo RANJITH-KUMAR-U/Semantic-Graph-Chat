173f28e0fddf49d4e2c356d4e9db3106624e1f5f
Ranjith Kumar <ranjithkumarranjithkumar8636@gmail.com>
2026-08-18 19:52:46 +0530

    fix(next): remove unrecognized top-level outputFileTracingRoot from next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,6 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  outputFileTracingRoot: path.join(__dirname, '../..'),
+  reactStrictMode: true,
 };
+
 module.exports = nextConfig;
